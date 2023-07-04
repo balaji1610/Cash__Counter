02c7775fe51526ecbf7f5974a61cbc2b5efdef17
balaji1610 <balajiece1610@gmail.com>
2023-07-04 10:40:05 +0530

    install_axios

--- src/Pages/HistoryPage.jsx
+++ src/Pages/HistoryPage.jsx
@@ -1,3 +1,17 @@
+import axios from "axios";
+import { useEffect } from "react";
 export default function HistoryPage() {
+  useEffect(() => {
+    async function GetResult() {
+      const result = await axios(
+        "https://orange-salty-insect.glitch.me/cashcounter"
+      );
+
+      console.log(result.data);
+    }
+
+    GetResult();
+  }, []);
+
   return <div>HistoryPage</div>;
 }
