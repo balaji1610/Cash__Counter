6473cb28d61e43e649410933b92146b28ad48c50
balaji1610 <balajiece1610@gmail.com>
2023-07-04 18:27:28 +0530

    Delete_method

--- src/Services/ServicesAxios.js
+++ src/Services/ServicesAxios.js
@@ -26,6 +26,31 @@ const Services = {
 
     return response;
   },
+
+  putApi: (id, payload) => {
+    const response = axios
+      .put(`${BASE_URL}/${id}`, payload)
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        return error;
+      });
+
+    return response;
+  },
+  DeleteApi: (id) => {
+    const response = axios
+      .delete(`${BASE_URL}/${id}`)
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        return error;
+      });
+
+    return response;
+  },
 };
 
 export default Services;
