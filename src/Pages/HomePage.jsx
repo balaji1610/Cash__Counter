9346e16de5d21734692dcc6590de27b15605af1c
balaji1610 <balajiece1610@gmail.com>
2023-07-16 20:43:14 +0530

    Design_a_footer

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import CashCounterPage from "./CashCounterPage";
 import HistoryPage from "./HistoryPage";
 import Header from "../Containers/Header";
-
+import Footer from "../Containers/Footer";
 export default function HomePage() {
   return (
     <div>
@@ -11,6 +11,7 @@ export default function HomePage() {
         <Route path="/" element={<CashCounterPage />} />
         <Route path="/history" element={<HistoryPage />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
